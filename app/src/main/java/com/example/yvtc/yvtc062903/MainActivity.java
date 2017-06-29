fb91b1a139944e9f0bb9247f0b0939544f0b166f
Shaojung <shaojung@gmail.com>
2017-06-29 13:29:38 +0800

    programilly add button on layout

--- app/src/main/java/com/example/yvtc/yvtc062903/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc062903/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout1);
+        Button btn3 = new Button(MainActivity.this);
+        btn3.setText("Button3");
+        layout.addView(btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is button3", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void click1(View v)
